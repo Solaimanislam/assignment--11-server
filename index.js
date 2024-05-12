59476d62259f94108f8d4e7e030fc89bbf853c63
Solaimanislam <islamsi2001@gmail.com>
2024-05-13 02:22:15 +0600

    added get method

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 
-const corsOption = {
-    origin: ['http://localhost:5173/'],
-    Credential: true,
-    optionSuccessStatus: 200
-}
+// const corsOption = {
+//     origin: ['http://localhost:5173/'],
+//     Credential: true,
+//     optionSuccessStatus: 200
+// }
 
 console.log(process.env.DB_PASS);
 
-app.use(cors(corsOption))
+app.use(cors())
 app.use(express.json())
 
 
@@ -38,12 +38,25 @@ async function run() {
 
     const serviceCollection = client.db('homeService').collection('services');
 
-    // get all service data from db
-    app.get('/service', async(req, res) => {
-        const result = await serviceCollection.find()
-
+    app.get('/services', async(req, res) => {
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
         res.send(result);
     })
+
+    app.post('/services', async(req, res)=> {
+        const newService = req.body;
+        console.log(newService);
+        const result = await serviceCollection.insertOne(newService);
+        res.send(result);
+    })
+
+    // get all service data from db
+    // app.get('/service', async(req, res) => {
+    //     const result = await serviceCollection.find()
+
+    //     res.send(result);
+    // })
 
 
 
